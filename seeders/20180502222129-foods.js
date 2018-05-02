7c558b48fca8ff9b0ced32878a5cb2268a29c692
Anna Lewis <anna.noel.lewis@gmail.com>
2018-05-02 17:36:51 -0600

    Add food seed data

--- seeders/20180502222129-foods.js
+++ seeders/20180502222129-foods.js
@@ -1,17 +1,23 @@
+var fakerFoods = require('../public/javascripts/fakerFoods')
 'use strict';
 
 module.exports = {
   up: (queryInterface, Sequelize) => {
-    
+    var foods = []
+    for (var i = 0; i < 50; i++) {
+      var randomIndex = Math.floor(Math.random() * fakerFoods.length)
+      var randomName = fakerFoods[randomIndex];
+      var randomCalories = Math.floor(Math.random() * 2000);
+      foods.push({
+        name: randomName,
+        calories: randomCalories,
+        createdAt: new Date(),
+        updatedAt: new Date()
+      })
+    }
 
-    return queryInterface.bulkInsert('Food', [{
-      name: 'waffle',
-      calories: 200,
-      createdAt: new Date(),
-      updatedAt: new Date()
-    }]
-
-  )},
+    return queryInterface.bulkInsert('Food', foods)
+  },
 
   down: (queryInterface, Sequelize) => {
     /*
