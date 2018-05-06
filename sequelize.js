2e7f16e682ae3e6fbae6590395c56b57e511f4ff
Anna Lewis <anna.noel.lewis@gmail.com>
2018-05-05 21:35:54 -0600

    Update sequelize config

--- sequelize.js
+++ sequelize.js
@@ -2,6 +2,15 @@ var Sequelize = require('sequelize');
 var env = process.env.NODE_ENV || 'development';
 var config = require('./config/config.js')[env];
 
-module.exports = new Sequelize(config.database, config.username, config.password, config, {
-  logging: console.log
-});
+const db = () => {
+  if(config.url) {
+    new Sequelize(config.url, {dialect: 'postgres'})
+  } else {
+    new Sequelize(config.database, config.username, config.password, {
+      host: config.host,
+      dialect: 'postgres',
+      logging: console.log
+    })
+  }}
+
+module.exports = db
