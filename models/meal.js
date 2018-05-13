bffe7ab7b3e852915fc6ea521733d1c06cd02250
Anna Lewis <anna.noel.lewis@gmail.com>
2018-05-12 19:11:51 -0600

    All tests passing for meal endpoints

--- models/meal.js
+++ models/meal.js
@@ -1,15 +1,17 @@
-const { STRING } = require('sequelize');
-const models = require('./index')
+'use strict';
+module.exports = (sequelize, DataTypes) => {
+    var Meal = sequelize.define('Meal', {
+        name: DataTypes.STRING
+    });
 
-module.exports = sequelize => {
-  var Meal = sequelize.define('Meal', {
-    name: STRING
-  });
-  Meal.associate = function(models) {
-    Meal.belongsToMany(models.Food, {
-      through: models.FoodMeals,
-      as: 'foods',
-      foreignKey: 'foodId' })
-  };
-  return Meal;
-};
+    Meal.associate = function (models) {
+        Meal.belongsToMany(models.Food, {
+            through: models.FoodMeals,
+            as: 'foods',
+            foreignKey: 'foodId'
+        })
+    };
+
+    return Meal;
+}
+;
--- routes/api/v1/meals.js
+++ routes/api/v1/meals.js
@@ -3,11 +3,19 @@ const router = express.Router();
 const meals = require('../../../controllers/meals')
 
 router.get('/', function (req, res) {
-  getMeals(req, res)
+    getMeals(req, res)
 });
 
 router.get('/:id', function (req, res) {
-  getMeal(req, res)
+    getMeal(req, res)
+});
+
+router.post('/:id/foods/:foodId', function (req, res) {
+    postFoodToMeal(req, res)
+});
+
+router.delete('/:id/foods/:foodId', function (req, res) {
+    deleteFoodFromMeal(req, res)
 });
 
 module.exports = router;
